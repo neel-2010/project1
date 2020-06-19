8dd553ba9d63ee1ecf92cbe688832650e2200c73
neelpatel2010 <64746165+neelpatel2010@users.noreply.github.com>
2020-06-19 19:42:49 +0530

    till sign up and index

--- application.py
+++ application.py
@@ -1,9 +1,18 @@
 from flask import *
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return render_template("index.html")
 
-if __name__=='__main__':
-	app.run(debug=True)
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+
+if __name__ == '__main__':
+    app.run()
