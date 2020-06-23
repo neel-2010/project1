8f0ce00af94ad753f9843e9f8595c6ee7bce30c6
neelpatel2010 <64746165+neelpatel2010@users.noreply.github.com>
2020-06-23 18:51:39 +0530

    till login front-end

--- application.py
+++ application.py
@@ -2,17 +2,16 @@ from flask import *
 
 app = Flask(__name__)
 
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
-
-
 @app.route('/signup')
 def signup():
     return render_template("signup.html")
-
+@app.route('/login', methods=["POST","GET"])
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run()
